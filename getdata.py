ba6df9dcaf3b1df7f03b6be9f44a2913215c5d42
Brennan Kelley <brennankelley@Brennans-MacBook-Pro.local>
2018-07-08 18:44:31 -0500

    time

--- getdata.py
+++ getdata.py
@@ -9,9 +9,7 @@ class day:
         self.date = date
         self.timeframe = timeframe
         self.table = self.getTable(date)
-        self.data = []
-
-        self.processTable(self.table, self.data)
+        self.data = self.processTable(self.table, self.data)
 
     def getTable(self, date):
         url = "https://www.wunderground.com/hourly/us/mn/victoria/date/" + date + "/KMNVICTO2?cm_ven=localwx_hour"
@@ -24,20 +22,33 @@ class day:
 
     def processTable(self, table, data):
         for row in table:
+            #find each cell in each row
             cells = row.findAll('td')
+            #eliminate the empty rows/cells
             if len(cells) > 0:
                 hour = {"time":0, "temp":0, "wind":0, "condition":0}
-
                 hour['time'] = cells[0].text.encode('utf-8').strip()
                 hour['condition'] = cells[1].text.encode('utf-8').strip().split("\n")[0]
                 hour['temp'] = cells[2].text.encode('utf-8').strip().split(" \xc2\xb0F")[0]
                 hour['wind'] = cells[9].text.encode('utf-8').strip()
-
-                print hour['temp']
-
                 data.append(hour)
-        print data
+        return data
 
 
 
-day(str(datetime.date.today()), 3)
+#day(str(datetime.date.today()), 3)
+
+
+
+
+
+def convert_to_24(time):
+
+    if time[-2:] == 'am':
+        return time[:(len(time)-6)]
+
+    if time[-2:] == 'pm':
+        return int(time[:(len(time)-6)])+12
+
+
+print convert_to_24("5:00 am")
