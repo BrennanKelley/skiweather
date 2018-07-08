089d27ec80cdbec41565eb778e18fdd40308c00a
Brennan Kelley <brennankelley@Brennans-MacBook-Pro.local>
2018-07-08 18:11:58 -0500

    classes

--- getdata.py
+++ getdata.py
@@ -1,33 +1,43 @@
 # import libraries
 import requests
-
-page = requests.get("https://www.wunderground.com/hourly/us/mn/victoria/KMNVICTO2?cm_ven=localwx_hour")
-#print page.content
-
+import datetime
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(page.content, 'html.parser')
 
-#table = soup.find("table", id='hourly-forecast-table')
+class day:
+    """A class containing 1 day of weather"""
+    def __init__(self, date, timeframe):
+        self.date = date
+        self.timeframe = timeframe
+        self.table = self.getTable(date)
+        self.data = []
 
-#print table
+        self.processTable(self.table, self.data)
 
-table = soup.select("#hourly-forecast-table tr")
+    def getTable(self, date):
+        url = "https://www.wunderground.com/hourly/us/mn/victoria/date/" + date + "/KMNVICTO2?cm_ven=localwx_hour"
+        page = requests.get(url)
+        #parse HTML
+        soup = BeautifulSoup(page.content, 'html.parser')
+        #find the table
+        table = soup.select("#hourly-forecast-table tr")
+        return table
+
+    def processTable(self, table, data):
+        for row in table:
+            cells = row.findAll('td')
+            if len(cells) > 0:
+                hour = {"time":0, "temp":0, "wind":0, "condition":0}
+
+                hour['time'] = cells[0].text.encode('utf-8').strip()
+                hour['condition'] = cells[1].text.encode('utf-8').strip().split("\n")[0]
+                hour['temp'] = cells[2].text.encode('utf-8').strip().split(" \xc2\xb0F")[0]
+                hour['wind'] = cells[9].text.encode('utf-8').strip()
+
+                print hour['temp']
+
+                data.append(hour)
+        print data
 
 
-for t in table:
-    cells = t.findAll('td')
-    if len(cells) > 0:
-        time = cells[0].text.encode('utf-8').strip()
-        temp = cells[2].text.encode('utf-8').strip()
-        wind = cells[9].text.encode('utf-8').strip()
-        print "{0},  {1},  {2}".format(time,temp,wind)
 
-
-# #for t in table:
-# cells = table[2].findAll('td')
-# print len(cells)
-# if len(cells) > 0:
-#     time = cells[0].text.encode('utf-8').strip()
-#     temp = cells[2].text.encode('utf-8').strip()
-#     wind = cells[9].text.encode('utf-8').strip()
-#     print "{0},  {1},  {2}".format(time,temp,wind)
+day(str(datetime.date.today()), 3)
